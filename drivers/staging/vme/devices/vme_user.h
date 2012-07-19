5c7754935cb54c0cf36f8007b0c5c770ed245094
Wolfgang Mauerer <wolfgang.mauerer@siemens.com>
2012-07-19 18:00:37 +0200

    vme_user: Add support for sending RMW cycles
    Export vme_master_rmw() via the userland driver.
    
    Signed-off-by: Wolfgang Mauerer <wolfgang.mauerer@siemens.com>

--- drivers/staging/vme/devices/vme_user.h
+++ drivers/staging/vme/devices/vme_user.h
@@ -11,8 +11,8 @@ struct vme_master {
 	unsigned long long vme_addr;	/* Starting Address on the VMEbus */
 	unsigned long long size;	/* Window Size */
 	u32 aspace;			/* Address Space */
-	u32 cycle;		/* Cycle properties */
-	u32 dwidth;		/* Maximum Data Width */
+	u32 cycle;			/* Cycle properties */
+	u32 dwidth;			/* Maximum Data Width */
 #if 0
 	char prefetchEnable;		/* Prefetch Read Enable State */
 	int prefetchSize;		/* Prefetch Read Size (Cache Lines) */
@@ -35,7 +35,7 @@ struct vme_slave {
 	unsigned long long vme_addr;	/* Starting Address on the VMEbus */
 	unsigned long long size;	/* Window Size */
 	u32 aspace;			/* Address Space */
-	u32 cycle;		/* Cycle properties */
+	u32 cycle;			/* Cycle properties */
 #if 0
 	char wrPostEnable;		/* Write Post State */
 	char rmwLock;			/* Lock PCI during RMW Cycles */
@@ -48,11 +48,17 @@ struct vme_irq_id {
 	__u8 statid;
 };
 
+struct vme_rmw {
+	unsigned int mask;
+	unsigned int compare;
+	unsigned int swap;
+};
+
 #define VME_GET_SLAVE _IOR(VME_IOC_MAGIC, 1, struct vme_slave)
 #define VME_SET_SLAVE _IOW(VME_IOC_MAGIC, 2, struct vme_slave)
 #define VME_GET_MASTER _IOR(VME_IOC_MAGIC, 3, struct vme_master)
 #define VME_SET_MASTER _IOW(VME_IOC_MAGIC, 4, struct vme_master)
 #define VME_IRQ_GEN _IOW(VME_IOC_MAGIC, 5, struct vme_irq_id)
+#define VME_RMW _IOW(VME_IOC_MAGIC, 6, struct vme_rmw)
 
 #endif /* _VME_USER_H_ */
-
